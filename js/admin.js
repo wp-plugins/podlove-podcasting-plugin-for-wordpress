7cc0c53c464e58867d345e39ae7ef391b7777810
eteubert <eteubert@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-20 11:28:56 +0000

    git-svn-id: https://plugins.svn.wordpress.org/podlove-podcasting-plugin-for-wordpress/trunk@575094 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/admin.js
+++ js/admin.js
@@ -15,94 +15,16 @@ function human_readable_size(size) {
 
 jQuery(function($) {
 
-	// live preview for media file url templates in settings
-	function update_media_file_preview() {
-		$('input[name*="url_template"]').each(function() {
-			var template = $(this).val();
-			var $preview = $(this).closest('td').find('.url_template_preview');
-			var $container = $(this).closest('table');
+	$("#podlove_settings_handle_validation").each(function() {
+		DashboardValidation.init($(this));
+	});
 
-			var media_file_base_uri = $('#podlove_show_media_file_base_uri').val();
-			var episode_slug        = 'example-episode';
-			var feed_suffix         = $container.find('[name*="suffix"]').val();
+	$(".postbox[id*=podlove_show]").each(function() {
+		Episode.init($(this));
+	});
 
-			var selected_format     = $container.find('[name*="media_format_id"] option:selected').text();
-			var format_extension    = selected_format.match(/\((.*)\)/)[1];
-
-			template = template.replace( '%media_file_base_url%', media_file_base_uri );
-			template = template.replace( '%episode_slug%', episode_slug );
-			template = template.replace( '%suffix%', feed_suffix );
-			template = template.replace( '%format_extension%', format_extension );
-
-			$preview.html(template);	
-		});
-	}
-
-	$('input[name*="url_template"]').on( 'keyup', update_media_file_preview );
-	$('input[name*="suffix"]').on( 'keyup', update_media_file_preview );
-	$('#podlove_show_media_file_base_uri').on( 'keyup', update_media_file_preview );
-	$('[name*="media_format_id"]').on( 'change', update_media_file_preview );
-	update_media_file_preview();
-
-	// live preview for file urls
-	var	update_media_file_path = function() {
-		$("tr.row_media_locations td .media_file_path").each(function() {
-			$container = $(this).closest('.inside');
-			$checkbox  = $(this).parent().find("input");
-
-			if ($($checkbox).is(":checked")) {
-				var url                 = $checkbox.data('template');
-
-				var media_file_base_uri = $container.find('input[name="show-media-file-base-uri"]').val();
-				var episode_slug        = $container.find('input[name*="slug"]').val();
-				var feed_suffix         = $checkbox.data('suffix');
-				var format_extension    = $checkbox.data('extension');
-				var size                = $checkbox.data('size');
-
-				url = url.replace( '%media_file_base_url%', media_file_base_uri );
-				url = url.replace( '%episode_slug%', episode_slug );
-				url = url.replace( '%suffix%', feed_suffix );
-				url = url.replace( '%format_extension%', format_extension );
-
-				output = '(' + url + ' [' + human_readable_size( size ) + '])';
-			} else {
-				output = "";
-			}
-			$(this).html(output);
-		});
-	}
-	
-	$("tr.row_media_locations td label").after('<span class="media_file_path"></span>');
-	update_media_file_path();
-	$('input[name*="slug"], input[name*="media_locations"]').on('change', update_media_file_path);
-
-	$("#validate_everything").click(function(e) {
-		e.preventDefault();
-
-		$(".release .file").each(function() {
-			var file_id = $(this).data('id');
-
-			var data = {
-				action: 'podlove-validate-file',
-				file_id: file_id
-			};
-
-			$.ajax({
-				url: ajaxurl,
-				data: data,
-				dataType: 'json',
-				success: function(result) {
-					$file = $('.file[data-id="' + result.file_id + '"]');
-					console.log(result);
-					if (result.reachable) {
-						$(".status", $file).html("<span style='color:green'>ok</span>");
-					} else {
-						$(".status", $file).html("<span style='color:red'>unreachable</span>");
-					}
-				}
-			});
-
-		});
+	$("#podlove_media_locations").each(function() {
+		MediaLocationSettings.init($(this));
 	});
 	
 });
