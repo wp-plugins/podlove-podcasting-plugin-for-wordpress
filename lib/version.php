0e944f2cd917392bc8c38289b22797d1c7ac6a8e
eteubert <eteubert@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-15 09:46:06 +0000

    remove deleted files
    git-svn-id: https://plugins.svn.wordpress.org/podlove-podcasting-plugin-for-wordpress/trunk@948672 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/version.php
+++ lib/version.php
@@ -40,7 +40,7 @@
 namespace Podlove;
 use \Podlove\Model;
 
-define( __NAMESPACE__ . '\DATABASE_VERSION', 78 );
+define( __NAMESPACE__ . '\DATABASE_VERSION', 79 );
 
 add_action( 'init', '\Podlove\run_database_migrations' );
 
@@ -57,6 +57,13 @@ function run_database_migrations() {
 			\Podlove\run_migrations_for_version( $i );
 			update_option( 'podlove_database_version', $i );
 		}
+
+		// flush rewrite rules after migrations
+		set_transient( 'podlove_needs_to_flush_rewrite_rules', true );
+
+		// purge cache after migrations
+		$cache = \Podlove\Cache\TemplateCache::get_instance();
+		$cache->setup_purge();
 	}
 
 }
@@ -882,6 +889,11 @@ function run_migrations_for_version( $version ) {
 				$c->url_scheme = 'https://auphonic.com/donate_credits?user=%account-placeholder%';
 				$c->save();
 			}
+		break;
+		case 79:
+			set_transient( 'podlove_needs_to_flush_rewrite_rules', true );
+			$cache = \Podlove\Cache\TemplateCache::get_instance();
+			$cache->setup_purge();
 		break;
 	}
 
